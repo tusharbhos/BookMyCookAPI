d5dade00f589eb7cc31d0184c78cfa4355bbae54
siddhesh jadhav <siddheshvjadhav20@gmail.com>
2024-05-27 23:02:51 +0530

    siddhesh - Server added

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-let app = require('express')
+// All imports
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import fs from 'fs'
+import cors from 'cors'
+
+
+// routes import here
+
+
+dotenv.config()
+let PORT = process.env.PORT || 3113
+let app = express()
+
+// log the app activity
+app.use(morgan('common', { stream: fs.createWriteStream('./app.log') }))
+
+// Cors
+app.use(cors())
+
+// Body Parser
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+app.get('/health', async (req, res) => {
+    res.send("<h1>Health is ok</h1>")
+})
+
+app.listen(PORT, (err) => {
+    if (err) throw new Error(err)
+    console.log(`Port ${PORT} is running`)
+})
